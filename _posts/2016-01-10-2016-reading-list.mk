c4619727e44d1df4a3d9fdc4231a65bfbf8c4b6f
alexcacciamani <alexcacciamani@users.noreply.github.com>
2016-01-10 12:27:01 -0500

    Update 2016-01-10-2016-reading-list.mk

--- _posts/2016-01-10-2016-reading-list.mk
+++ _posts/2016-01-10-2016-reading-list.mk
@@ -4,6 +4,9 @@ title: "2016 Reading List"
 date: 2016-01-10
 ---
 
-The following are a few books I'll be getting to this year in no specific order:
+<p>The following are a few books I'll be getting to this year in no specific order:</p>
 
-"Elon Musk" by Ashlee Vance
+_"Elon Musk"_
+by Ashlee Vance
+
+
